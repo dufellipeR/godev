7a9af61f85abf095af7fc96bb9dd362f5b61a177
dufellipeR <eduardo.fellipe30@gmail.com>
2021-02-18 19:23:14 -0300

    Adding room register

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,14 +1,13 @@
 import { container } from 'tsyringe';
 
-import '@modules/users/providers';
-import './providers';
-
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import IAppointmentsRepository from '@modules/person/infra/typeorm/repositories/node_modules/@modules/appointments/repositories/IAppointmentsRepository';
+import RoomRepository from '@modules/room/infra/typeorm/repositories/RoomRepository';
+import IRoomRepository from '@modules/room/repositories/IRoomRepository';
 
 
-container.registerSingleton<IAppointmentsRepository>(
-  'AppointmentsRepository',
-  AppointmentsRepository,
+container.registerSingleton<IRoomRepository>(
+  'RoomRepository',
+  RoomRepository,
 );
+
+
 
