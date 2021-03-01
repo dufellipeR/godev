859be0e5b462ee0a017457e987cc10c11d27b0f6
dufellipeR <eduardo.fellipe30@gmail.com>
2021-03-01 03:06:53 -0300

    Registering Person Module

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -2,12 +2,11 @@ import { container } from 'tsyringe';
 
 import RoomRepository from '@modules/room/infra/typeorm/repositories/RoomRepository';
 import IRoomRepository from '@modules/room/repositories/IRoomRepository';
+import IPersonRepository from '@modules/person/repositories/IPersonRepository';
+import PersonRepository from '@modules/person/infra/typeorm/repositories/PersonRepository';
 
-
-container.registerSingleton<IRoomRepository>(
-  'RoomRepository',
-  RoomRepository,
+container.registerSingleton<IRoomRepository>('RoomRepository', RoomRepository);
+container.registerSingleton<IPersonRepository>(
+  'PersonRepository',
+  PersonRepository,
 );
-
-
-
